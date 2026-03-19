882fc7584c514d4887473272173b24d41fdcc04d
Ismail <ismahm01@easv365.dk>
2026-03-19 15:53:01 +0100

    Added login window to extension

---
+++ popup.js
@@ -0,0 +1,21 @@
+let signInBtn = document.getElementById("signInBtn");
+let signInUserField = document.getElementById("signInUserField");
+let signInPassField = document.getElementById("signInPassField")
+const MOCK_USER = "worker";
+const MOCK_PASS = "worker123"
+
+
+signInBtn.addEventListener('click', () => {
+
+    if (signInUserField.value == MOCK_USER && signInPassField.value == MOCK_PASS) {
+
+        console.log("LOGGED IN")
+        window.location.href = "./main_page.html"
+    }
+    else {
+
+        console.log("INCORRECT CREDENTIALS" + signInPassField.value)
+
+    }
+
+})
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Add paths to any files where you might write Tailwind classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
